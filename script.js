05b9c6f5423529525b4f4b4c5bb76ed6fa7d42de
Pongtorn Angsuchotmetee <monkeydouy@pongtorn-tangerine.local>
2022-03-18 20:28:57 +0700

    init

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
-const getMobileOS = () => {
+const fackbookClick = () => {
   const ua = navigator.userAgent;
   if (/android/i.test(ua)) {
-    return "Android";
+    // "Android";
+    window.location = "fb://page/109125357475042";
   } else if (
     /iPad|iPhone|iPod/.test(ua) ||
     (navigator.platform === "MacIntel" && navigator.maxTouchPoints > 1)
   ) {
-    return "iOS";
+    window.location = "fb://profile/109125357475042";
+    //return "iOS";
   }
-  return "Other";
+  //return "Other";
+  window.location = "fb://page/109125357475042";
 };
