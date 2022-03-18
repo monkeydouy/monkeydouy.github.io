b2e99c7314e274efe1556341632b6e30dd675558
Pongtorn Angsuchotmetee <monkeydouy@pongtorn-tangerine.local>
2022-03-18 20:33:58 +0700

    init

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-const fackbookClick = () => {
+const LinkOnLoad = () => {
   const ua = navigator.userAgent;
   if (/android/i.test(ua)) {
     // "Android";
-    window.location = "fb://page/109125357475042";
+    document.getElementById("facebook").href = "fb://page/109125357475042";
   } else if (
     /iPad|iPhone|iPod/.test(ua) ||
     (navigator.platform === "MacIntel" && navigator.maxTouchPoints > 1)
   ) {
-    window.location = "fb://profile/109125357475042";
+    document.getElementById("facebook").href = "fb://profile/109125357475042";
     //return "iOS";
   }
   //return "Other";
-  window.location = "fb://page/109125357475042";
+  document.getElementById("facebook").href = "fb://page/109125357475042";
 };
